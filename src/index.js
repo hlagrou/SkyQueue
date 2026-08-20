94362ab33076af9e4077e90f37ebb9e7373ebecd
hlagrou <109159121+hlagrou@users.noreply.github.com>
2026-08-20 13:08:26 -0400

    Added the Axios API and edited the JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function refreshWeatherTemp(response) {
+  console.log(response.data);
+  let currentTemperature = document.querySelector("#presenttemperature");
+  let cityInput = document.querySelector("#nameCity");
+
+  cityInput.innerHTML = response.data.city;
+
+  let temperature = response.data.temperature.current;
+  currentTemperature.innerHTML = `${Math.round(temperature)}℃`;
+}
+
+function searchCity(city) {
+  let apiKey = "t0da92baba343cd544ba68bf04f77ao7";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeatherTemp);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let cityInput = document.querySelector("#nameCity");
-  cityInput.innerHTML = searchInput.value;
+
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
 }
 
 let weatherSearchForm = document.querySelector("#search-form");
-weatherSearchForm.addEventListener("Submit", handleSearchSubmit);
+weatherSearchForm.addEventListener("submit", handleSearchSubmit);
